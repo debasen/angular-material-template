842e8a6f0d0949787c7d1849cd6f710850a47879
Debapriya Sen <debapriya.sen93@gmail.com>
2021-05-02 15:22:20 +0530

    Bootstrap Style reset updated. Side navigation overlay issue fixed.

--- src/app/main/forms/basic-form/basic-form.component.ts
+++ src/app/main/forms/basic-form/basic-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, NgForm, Validators } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
 import {ThemePalette} from '@angular/material/core';
 
 @Component({
@@ -10,21 +11,14 @@ import {ThemePalette} from '@angular/material/core';
 export class BasicFormComponent implements OnInit {
   //.ts for credit card form starts//
   creditCardForm = {
-    methods : {
-      fetchMonth: function () {
-        var result = [];
-        for (let i = 1; i <= 12; i++) {
-          result.push(i);
-        }
-        return result;
-      }, fetchYear: function () {
-        var result = [];
-        for (let i = 2010; i <= 2050; i++) {
-          result.push(i);
-        };
-        return result;
+    months : [...Array(12).keys()],
+    years : (function(){
+      let arr = [];
+      for(let i=1;i<12;i++){
+        arr.push(i);
       }
-    }
+      return arr;
+    })()
   };
   //.ts for credit card form ends//
   //.ts for inline form starts//
@@ -43,10 +37,48 @@ export class BasicFormComponent implements OnInit {
   };
   
   //.ts for validate states form ends//
+  fieldAppearance = 'standard';
+  passwordHide = true;
+  cnfPasswordHide = true;
+  languages = ['English','Hindi'];
 
   constructor() { }
 
   ngOnInit(): void { 
+  }
+
+  validateConfirmPassword(userRegistrationForm:NgForm){
+    if(!userRegistrationForm.controls['confirmPassword'].value || userRegistrationForm.controls['confirmPassword'].value==""){
+        delete userRegistrationForm.controls['confirmPassword'].errors?.passwordMismatch;
+    }
+    else if(userRegistrationForm.controls['confirmPassword'].value !== userRegistrationForm.controls['password'].value){
+      userRegistrationForm.controls['confirmPassword'].setErrors({'passwordMismatch': true});
+    }else{
+      userRegistrationForm.controls['confirmPassword'].setErrors(null);
+    }
+  }
+
+  addLanguage(event : MatChipInputEvent){
+    const input = event.input;
+    const value = event.value;
+    // Add our language
+    if ((value || '').trim()) {
+      this.languages.push(value.trim());
+    }
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeLanguage(language: string): void {
+    const index = this.languages.indexOf(language);
+    if (index >= 0) {
+      this.languages.splice(index, 1);
+    }
+  }
+  submit(form: NgForm){
+    console.log(form);
   }
   
 
--- src/app/shared/letter-icon/letter-icon.component.ts
+++ src/app/shared/letter-icon/letter-icon.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-letter-icon',
+  selector: 'amt-letter-icon',
   templateUrl: './letter-icon.component.html',
   styleUrls: ['./letter-icon.component.scss']
 })
